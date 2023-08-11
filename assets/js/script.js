362e6f8fd27d72a184cbdc935d48e3ff0bbfa193
coltondane <113216904+coltondane@users.noreply.github.com>
2023-08-11 15:00:47 -0600

    added display date function and re-linked css and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// variables
+var todaysDateEl = document.querySelector("#currentDay");
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -20,4 +23,10 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    function dateDisplay() {
+      var date = dayjs().format('MMM DD, YYYY');
+      todaysDateEl.textContent = "Todays Date: " + date;
+      console.log(date);
+    }
+    dateDisplay();
   });

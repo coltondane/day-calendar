2263d1a80768d33d1cd92ee4f1afd5d7213bbf5f
coltondane <113216904+coltondane@users.noreply.github.com>
2023-08-15 13:19:59 -0600

    gathered value from text area that pairs to selected save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // variables
 var todaysDateEl = document.querySelector("#currentDay");
+var containerEl = document.querySelector("#container");
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -11,17 +12,30 @@ $(function () {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
+    function getTextValue(event) {
+      if (event.target.nodeName === "BUTTON") {
+        var textArea = event.target.previousElementSibling.value
+        console.log(textArea);
+      }
+      else if (event.target.nodeName === "I") {
+        var textArea = event.target.parentNode.previousElementSibling.value
+        console.log(textArea);
+      }
+    }
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    function applyCSS() {
+
+    }
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+    function getLocal() {
+      
+    }
     // Add code to display the current date in the header of the page.
     function dateDisplay() {
       var date = dayjs().format('MMM DD, YYYY');
@@ -29,4 +43,8 @@ $(function () {
       console.log(date);
     }
     dateDisplay();
+
+    // add an event listener for the save buttons
+    containerEl.addEventListener("click", getTextValue);
+    console.log(containerEl);
   });
